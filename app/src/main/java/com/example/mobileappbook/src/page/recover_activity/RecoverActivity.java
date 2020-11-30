3b2f3360175149b2821feb44e72e1591d3366c9c
vietle2909 <vietle2809@gmail.com>
2020-12-01 03:01:04 +0700

    ấdfghh

--- app/src/main/java/com/example/mobileappbook/src/page/recover_activity/RecoverActivity.java
+++ app/src/main/java/com/example/mobileappbook/src/page/recover_activity/RecoverActivity.java
@@ -1,35 +1,33 @@
-package com.example.mobileappbook.src.page.login_activity;
+package com.example.mobileappbook.src.page.recover_activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobileappbook.R;
-import com.example.mobileappbook.src.page.recover_activity.RecoverActivity;
+import com.example.mobileappbook.src.page.login_activity.LoginActivity;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class RecoverActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        listenerOnclicked();
+        setContentView(R.layout.activity_recover);
 
+        listenerOnclicked();
     }
 
     private void listenerOnclicked() {
-        findViewById(R.id.txt_forgotPassword).setOnClickListener(this);
+        findViewById(R.id.btn_send).setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.txt_forgotPassword :
-                startActivity(new Intent(getApplicationContext(), RecoverActivity.class));
+            case R.id.btn_send :
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
                 break;
         }
