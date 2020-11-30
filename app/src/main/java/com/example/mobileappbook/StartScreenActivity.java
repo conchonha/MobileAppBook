e91dd1b2c4ebf249df9382ee8f5d88958e5d0450
vietle2909 <vietle2809@gmail.com>
2020-11-30 19:27:41 +0700

    va

--- app/src/main/java/com/example/mobileappbook/StartScreenActivity.java
+++ app/src/main/java/com/example/mobileappbook/StartScreenActivity.java
@@ -1,15 +1,45 @@
 package com.example.mobileappbook;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class StartScreenActivity extends AppCompatActivity {
+    private CardView mCardViewLogin, mCardViewRegister;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
+
+        init();
+        initView();
+    }
+
+    private void initView() {
+
+        // button login
+        mCardViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_login);
+            }
+        });
+        //button register
+        mCardViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_register);
+            }
+        });
+    }
+
+    private void init() {
+        mCardViewLogin = findViewById(R.id.btn_login);
+        mCardViewRegister = findViewById(R.id.btn_register);
     }
 }
