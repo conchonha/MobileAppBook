83c9a235737c3586c440858acc7309766b66590a
vietle2909 <vietle2809@gmail.com>
2020-12-08 01:59:23 +0700

    hghj

--- app/src/main/java/com/example/mobileappbook/src/fragment/fragment_cart/FragmentCart.java
+++ app/src/main/java/com/example/mobileappbook/src/fragment/fragment_cart/FragmentCart.java
@@ -1,5 +1,6 @@
 package com.example.mobileappbook.src.fragment.fragment_cart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,16 +8,35 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.mobileappbook.R;
+import com.example.mobileappbook.src.page.payment.PaymentActivity;
 
 public class FragmentCart extends Fragment {
     private View mView;
+    private CardView mCardPay;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_cart,container,false);
+        initView();
+        init();
         return mView;
+    }
+
+    private void init() {
+        mCardPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        mCardPay = mView.findViewById(R.id.card_pay);
     }
 }
