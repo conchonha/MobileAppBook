b76508f87ebb69e3fa2692ee9a5e67c3b79dea69
vietle2909 <vietle2809@gmail.com>
2020-12-15 01:24:48 +0700

    asd

--- app/src/main/java/com/example/mobileappbook/src/page/course/CreateCourse2Activity.java
+++ app/src/main/java/com/example/mobileappbook/src/page/course/CreateCourse2Activity.java
@@ -1,17 +1,38 @@
 package com.example.mobileappbook.src.page.course;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.mobileappbook.R;
 
 public class CreateCourse2Activity extends AppCompatActivity {
+    private CardView mBtnChangeCourseAvt;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course2);
+        initView();
+        listenerOnclick();
+    }
+
+    private void listenerOnclick() {
+        mBtnChangeCourseAvt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateCourse2Activity.this,ChangeCourseAvataActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void initView() {
+        mBtnChangeCourseAvt = findViewById(R.id.btn_next);
     }
 }
